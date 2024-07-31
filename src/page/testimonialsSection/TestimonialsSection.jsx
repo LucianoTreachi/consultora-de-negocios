21ef6ce9800ebe3ddfa16ff16c768bdc4a194afc
lucianotreachi <lucianotreachi@outlook.com>
2024-07-31 14:49:25 -0300

    Agregar icono de chevron right y left

--- src/page/testimonialsSection/TestimonialsSection.jsx
+++ src/page/testimonialsSection/TestimonialsSection.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { dataTestimonials } from "../../data/dataTestimonials";
 import TitleSection from "../../components/titleSection/TitleSection";
+import ChevronRightIcon from "../../icons/ChevronRightIcon";
+import ChevronLeftIcon from "../../icons/ChevronLeftIcon";
 import testimonialQuoteImage from "../../assets/testimonial-quote.png";
 import styles from "./TestimonialsSection.module.css";
 
@@ -45,19 +47,15 @@ export default function TestimonialsSection() {
               <div className={styles.controls}>
                 <button
                   onClick={handlePrevTestimonial}
-                  aria-label="Testimonio anterior"
+                  aria-label="Retroceder al testimonio anterior"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path>
-                  </svg>
+                  <ChevronRightIcon />
                 </button>
                 <button
                   onClick={handleNextTestimonial}
-                  aria-label="Testimonio siguiente"
+                  aria-label="Avanzar al siguiente testimonio"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path d="M13.1714 12.0007L8.22168 7.05093L9.63589 5.63672L15.9999 12.0007L9.63589 18.3646L8.22168 16.9504L13.1714 12.0007Z"></path>
-                  </svg>
+                  <ChevronLeftIcon />
                 </button>
               </div>
             </div>
