0a5c82d9f803696495290774d79e7fe19addab88
lucianotreachi <lucianotreachi@outlook.com>
2024-07-31 09:52:13 -0300

    Componetizar el elemento svg

--- src/page/aboutSection/AboutSection.jsx
+++ src/page/aboutSection/AboutSection.jsx
@@ -1,4 +1,5 @@
 import TitleSection from "../../components/titleSection/TitleSection";
+import ArrowRight from "../../icons/ArrowRight";
 import styles from "./AboutSection.module.css";
 
 export default function AboutSection() {
@@ -51,13 +52,7 @@ export default function AboutSection() {
               rel="noreferrer"
             >
               Solicitar asesoramiento
-              <svg
-                className={styles.arrow}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-              </svg>
+              <ArrowRight className={styles.arrow} />
             </a>
           </div>
         </div>
