341b848d0b8d1131321f7dc8c5cd1892163f6781
lucianotreachi <lucianotreachi@outlook.com>
2024-07-31 09:43:02 -0300

    Actualizar información y estilos

--- src/data/dataFaqs.jsx
+++ src/data/dataFaqs.jsx
@@ -5,7 +5,7 @@ export const dataFaqs = [
     question:
       "¿Cómo puedo saber si mi negocio se beneficiaría de los servicios de la consultora?",
     answer:
-      "Nos especializamos en evaluar negocios de diversas industrias y tamaños para determinar si se pueden beneficiar de nuestros servicios. Recomendamos una consulta inicial gratuita donde podamos discutir tus objetivos y desafíos comerciales. A través de esta conversación, podremos evaluar si nuestros servicios son adecuados para tu negocio y, de ser así, desarrollar un plan de trabajo personalizado que se adapte a tus necesidades.",
+      "Nuestros servicios están diseñados para negocios de todos los tamaños y sectores. Tras una evaluación inicial gratuita, identificaremos áreas clave de mejora y te mostraremos cómo podemos ayudarte a alcanzar tus objetivos.",
   },
 
   {
@@ -14,7 +14,7 @@ export const dataFaqs = [
     question:
       "¿La consultora brinda servicios de consultoría en línea o de manera presencial?",
     answer:
-      "Nos especializamos en evaluar negocios de diversas industrias y tamaños para determinar si se pueden beneficiar de nuestros servicios. Recomendamos una consulta inicial gratuita donde podamos discutir tus objetivos y desafíos comerciales. A través de esta conversación, podremos evaluar si nuestros servicios son adecuados para tu negocio y, de ser así, desarrollar un plan de trabajo personalizado que se adapte a tus necesidades.",
+      "Ofrecemos servicios tanto en línea como presenciales, adaptándonos a tus necesidades y preferencias. Nos aseguramos de proporcionar la mejor experiencia y resultados, independientemente del formato elegido.",
   },
 
   {
@@ -23,23 +23,15 @@ export const dataFaqs = [
     question:
       "¿Cuánto tiempo se necesita para ver resultados con los servicios de la consultora?",
     answer:
-      "Nos especializamos en evaluar negocios de diversas industrias y tamaños para determinar si se pueden beneficiar de nuestros servicios. Recomendamos una consulta inicial gratuita donde podamos discutir tus objetivos y desafíos comerciales. A través de esta conversación, podremos evaluar si nuestros servicios son adecuados para tu negocio y, de ser así, desarrollar un plan de trabajo personalizado que se adapte a tus necesidades.",
+      "El tiempo para ver resultados puede variar según la complejidad del negocio y los objetivos específicos. Sin embargo, muchos de nuestros clientes comienzan a notar mejoras significativas en sus procesos y rendimiento en unos pocos meses.",
   },
 
   {
     id: 4,
     number: "04",
-    question: "¿Cuál es el costo de contratar a la consultora?",
-    answer:
-      "Nos especializamos en evaluar negocios de diversas industrias y tamaños para determinar si se pueden beneficiar de nuestros servicios. Recomendamos una consulta inicial gratuita donde podamos discutir tus objetivos y desafíos comerciales. A través de esta conversación, podremos evaluar si nuestros servicios son adecuados para tu negocio y, de ser así, desarrollar un plan de trabajo personalizado que se adapte a tus necesidades.",
-  },
-
-  {
-    id: 5,
-    number: "05",
     question:
       "¿Cómo puedo beneficiarme de sus servicios si estoy comenzando mi emprendimiento?",
     answer:
-      "Nos especializamos en evaluar negocios de diversas industrias y tamaños para determinar si se pueden beneficiar de nuestros servicios. Recomendamos una consulta inicial gratuita donde podamos discutir tus objetivos y desafíos comerciales. A través de esta conversación, podremos evaluar si nuestros servicios son adecuados para tu negocio y, de ser así, desarrollar un plan de trabajo personalizado que se adapte a tus necesidades.",
+      "Ofrecemos asesoramiento personalizado para emprendedores, ayudándote a establecer una base sólida y desarrollar estrategias efectivas desde el inicio. Nuestros servicios te brindarán las herramientas y conocimientos necesarios para impulsar tu nuevo negocio hacia el éxito.",
   },
 ];
