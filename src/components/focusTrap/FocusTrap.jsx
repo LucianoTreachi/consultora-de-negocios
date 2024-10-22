1fe3afb4183e99014fa3379727aa17081bd04264
lucianotreachi <lucianotreachi@outlook.com>
2024-10-22 10:37:25 -0300

    Implementar componente FocusTrap para gestionar el foco

---
+++ src/components/focusTrap/FocusTrap.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef, cloneElement } from "react";
+
+const FocusTrap = ({ children, closeModal }) => {
+  const trapRef = useRef(null);
+  const firstElementRef = useRef(null);
+  const lastElementRef = useRef(null);
+
+  useEffect(() => {
+    const focusableElements = trapRef.current.querySelectorAll(
+      'a[href], button, textarea, input, select, [tabindex]:not([tabindex="-1"])'
+    );
+
+    if (focusableElements.length > 0) {
+      firstElementRef.current = focusableElements[0];
+      lastElementRef.current = focusableElements[focusableElements.length - 1];
+
+      trapRef.current.focus();
+
+      const handleKeyDown = (e) => {
+        if (e.key === "Tab") {
+          if (e.shiftKey) {
+            if (document.activeElement === firstElementRef.current) {
+              e.preventDefault();
+              lastElementRef.current.focus();
+            }
+          } else {
+            if (document.activeElement === lastElementRef.current) {
+              e.preventDefault();
+              firstElementRef.current.focus();
+            }
+          }
+        }
+
+        if (e.key === "Escape" && closeModal) {
+          closeModal();
+        }
+      };
+
+      document.addEventListener("keydown", handleKeyDown);
+
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }
+  }, [closeModal]);
+
+  return cloneElement(children, { ref: trapRef, tabIndex: "-1" });
+};
+
+export default FocusTrap;
