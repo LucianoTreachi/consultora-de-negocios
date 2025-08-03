730299fa82014165ddc4576acfd8f4af66ec37cb
lucianotreachi <lucianotreachi@outlook.com>
2025-08-03 12:50:37 -0300

    Reemplazar código SVG por componente reutilizable, en Modal y página del detalle del Servicio

--- src/components/modals/modalStartNow/ModalStartNow.jsx
+++ src/components/modals/modalStartNow/ModalStartNow.jsx
@@ -1,6 +1,7 @@
 import CloseIcon from "../../../icons/CloseIcon";
-import styles from "./ModalStartNow.module.css";
 import FocusTrap from "../../focusTrap/FocusTrap";
+import ArrowRight from "../../../icons/ArrowRight";
+import styles from "./ModalStartNow.module.css";
 
 export default function ModalStartNow({ closeModal }) {
   return (
@@ -29,14 +30,7 @@ export default function ModalStartNow({ closeModal }) {
               rel="noreferrer"
             >
               Solicitar asesoramiento
-              <svg
-                className={styles.arrow}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-              </svg>
+              <ArrowRight />
             </a>
           </div>
         </div>
--- src/pages/serviceDetail/ServiceDetail.jsx
+++ src/pages/serviceDetail/ServiceDetail.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { dataServices } from "../../data/dataServices";
 import NavigateToTop from "../../routing/NavigateToTop";
 import GoBackButton from "../../components/goBackButton/GoBackButton";
+import ArrowRight from "../../icons/ArrowRight";
 import Footer from "../../components/footer/Footer";
 import styles from "./ServiceDetail.module.css";
 
@@ -37,14 +38,7 @@ export default function ServiceDetail() {
                 rel="noreferrer"
               >
                 Solicitar asesoramiento
-                <svg
-                  className={styles.arrow}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                </svg>
+                <ArrowRight />
               </a>
             </div>
             <img
