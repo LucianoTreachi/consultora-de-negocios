626bebb9929f29215d6893e6079f3349b41d8bd5
lucianotreachi <lucianotreachi@outlook.com>
2025-08-04 08:55:37 -0300

    Reemplazar etiqueta <Link> por <a> en el componente Logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,16 +1,15 @@
-import { Link } from "react-router-dom";
 import logo from "/logo.png";
 import styles from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    <Link className={styles.logo} to="/">
+    <a className={styles.logo} href="/">
       <img
         src={logo}
         alt="Logo de Emmanuel Mansilla"
         width={100}
         height={100}
       />
-    </Link>
+    </a>
   );
 }
